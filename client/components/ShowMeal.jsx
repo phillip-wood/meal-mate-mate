b7f2ab727fe1d9776c1666eac695ebf6543a2c0b
student <student@eda-bc-03.localdomain>
2020-11-20 10:52:16 +1300

    not much

--- client/components/ShowMeal.jsx
+++ client/components/ShowMeal.jsx
@@ -3,8 +3,6 @@ import { connect } from 'react-redux'
 import Comment from './Comment'
 
 // imports the action
-// import { fetchMeals } from '../actions'
-
 
 
 export class ShowMeal extends React.Component {
@@ -13,12 +11,12 @@ export class ShowMeal extends React.Component {
   render () {
     return (
       <>
-        <div>
-        <img src="https://nt63xpepn03htbpb2sddvciv-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/img_3049-1080x720.jpg"></img>
-        <h2>whitebait fritters</h2>
-        <p>ingredients</p>
-        <p>description</p>
-        <p>author</p>
+        <div className='showMeal'>
+        <img className='showMealImg' src="https://nt63xpepn03htbpb2sddvciv-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/img_3049-1080x720.jpg"></img>
+        <h2 className="showMealName">whitebait fritters</h2>
+        <p className="showMealIngred">ingredients</p>
+        <p className="showMealDescrip">description</p>
+        <p className="showMealAuthor">author</p>
         </div>
 
         <Comment />
