a79e9bc35cee54c8d29e2e351ec80fffdd9d4878
student <student@eda-bc-03.localdomain>
2020-11-19 16:31:08 +1300

    fix seeds and migrations

--- server/db/migrations/20201119155109_meals.js
+++ server/db/migrations/20201119155109_meals.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('meals', table => {
         table.increments('id')
         table.string('mealName')
-        table.text('ingradients')
+        table.text('ingredients')
         table.text('description')
         table.text('imgURL')
         table.string('author')
