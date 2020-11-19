f16411fd0ea3a576ef9ebc7f0fa2a53e6fdc39b8
student <student@eda-bc-02.localdomain>
2020-11-19 16:09:09 +1300

    fix

--- server/db/seeds/comments.js
+++ server/db/seeds/comments.js
@@ -4,12 +4,12 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('comments').insert([
-        { id: 1, mealID: 1, comment: 'This is great' },
-        { id: 2, mealID: 2, comment: 'This Suxs!' },
-        { id: 3, mealID: 3, comment: 'Best Meal Ever!!!!' },
-        { id: 4, mealID: 1, comment: 'Tastes a bit bland' },
-        { id: 5, mealID: 2, comment: 'Who ever made this neeeds new tastes buds' },
-        { id: 6, mealID: 3, comment: 'Maybe use a bit more salt next time' }
+        { id: 1, meal_id: 1, comment: 'This is great' },
+        { id: 2, meal_id: 2, comment: 'This Suxs!' },
+        { id: 3, meal_id: 3, comment: 'Best Meal Ever!!!!' },
+        { id: 4, meal_id: 1, comment: 'Tastes a bit bland' },
+        { id: 5, meal_id: 2, comment: 'Who ever made this neeeds new tastes buds' },
+        { id: 6, meal_id: 3, comment: 'Maybe use a bit more salt next time' }
       ])
     })
 }
